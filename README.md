a508a086417706af158a07f61226b757f69d2b67
DrSmuc <107368724+DrSmuc@users.noreply.github.com>
2023-05-24 20:31:45 +0200

    completed user menager + write in file, leaderboard

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # Sah-V2-0
  
+
+
+                        S        A        H
+
+//onak lepo velki od tockica i crtica
+
+izbornik:
+  new game
+  match history and analasys
+  leaderboard
+  rules //optional
+  quit
+
+
+new game
+  user 1
+  user 2
+  .
+  .
+  .
+  guest
+  create new
+
+  ulazi u stari kod i igra se   //gulis
+    treba napravit provjeru za sah
+    provjerit pravila
+    stavit emoji za figure
+    + sprema u temp folder za analizu
+
+  analiza
+
+
+match history
+  sprema zadnjih 5 
+  korisri isti algoritam/funkciju kao za analizu
+
+
+leaderboard
+
+  user    wins
+  .        .
+  .        .
+  .        .
+
+
+rules  //optianal
+  print pravila ko u proslom
+
+
+quit 
+  return 0;
+  
--- Sah-V2-0.cpp
+++ Sah-V2-0.cpp
@@ -26,19 +26,8 @@ struct user
     char ime[51];
     short int win = 0;
     short int los = 0;
+    short int drw = 0;
 };
-
-void new_user(user* a, int& br_user)
-{
-    system("cls");
-    cout << "Ime novog igraca:" << endl;
-    cin.ignore();
-    cin.getline(a[br_user].ime, 51);
-    br_user++;
-}
-bool poIme(user a, user b) { return (strcmp(a.ime, b.ime)>0) ? true : false; }
-bool poWin(user a, user b) { return (a.win < b.win) ? true : false; }
-bool poLos(user a, user b) { return (a.los < b.los) ? true : false; }
 
 void pause()
 {
@@ -47,6 +36,49 @@ void pause()
     cin.ignore();
     getline(cin, dummy);
 }
+
+void new_user(user* a, int& br_user)
+{
+    bool ponoviti;
+    do
+    {
+        ponoviti = false;
+        system("cls");
+        cout << "Ime novog igraca:" << endl;
+        cin.ignore();
+        cin.getline(a[br_user].ime, 51);
+        for (int i = 0; i < br_user; i++)
+        {
+            if (strcmp(a[br_user].ime, a[i].ime) == 0)
+            {
+                cout << "Ime vec postoji, izaberite drugo!" << endl;
+                ponoviti = true;
+                pause();
+            }
+        }
+    } while (ponoviti);
+    br_user++;
+}
+
+void remove_user(user* a, int& br_user, char *name)
+{
+    for (int i = 0; i < br_user; i++)
+    {
+        if (strcmp(a[i].ime, name) == 0)
+        {
+            for (int j = i; j < br_user - 1; j++)
+            {
+                a[j] = a[j + 1];
+            }
+            return;
+        }
+    }
+}
+
+bool poIme(user a, user b) { return (strcmp(a.ime, b.ime)<0) ? true : false; }
+bool poWin(user a, user b) { return (a.win > b.win) ? true : false; }
+bool poLos(user a, user b) { return (a.los > b.los) ? true : false; }
+bool poDrw(user a, user b) { return (a.drw > b.drw) ? true : false; }
 
 int main()
 {
@@ -169,11 +201,13 @@ int main()
                     break;
             }
 
+            //stari kod
+
         }
 
         else if (izbor == 2)
         {
-
+            //prvo treba alg. za citanje
         }
 
         else if (izbor == 3)
@@ -184,10 +218,10 @@ int main()
             while (1)
             {
                 system("cls");
+                cout << "Ime\twin\tloss\tdraw" << endl;
+                cout << "====================================" << endl;
                 for (int i = 0; i < br_user; i++)
-                {
-                    cout << a[i].ime << "\t\t" << a[i].win << "\t\t" << a[i].los << endl;
-                }
+                    cout << a[i].ime << "\t" << a[i].win << "\t" << a[i].los << "\t" << a[i].drw << endl;
                 cout << endl;
                 cout << "1. sortiraj" << endl;
                 cout << "2. dodaj" << endl;
@@ -205,25 +239,38 @@ int main()
                         cout << "1. po imenu" << endl;
                         cout << "2. po pobjedama" << endl;
                         cout << "3. po porazima" << endl;
-                        cout << "4. povratak" << endl;
+                        cout << "4. po izjednacenjima" << endl;
+                        cout << "5. povratak" << endl;
                         cin >> menager_izbor;
 
-                        switch (menager_izbor)
+                        if (menager_izbor == 1)
                         {
-                        case 1:
                             sort(a, a + br_user, poIme);
                             break;
-                        case 2:
+                        }
+                        else if (menager_izbor == 2)
+                        {
                             sort(a, a + br_user, poWin);
                             break;
-                        case 3:
+                        }
+                        else if (menager_izbor == 3)
+                        {
                             sort(a, a + br_user, poLos);
                             break;
-                        case 4:
+                        }
+                        else if (menager_izbor == 4)
+                        {
+                            sort(a, a + br_user, poDrw);
+                            break;
+                        }
+                        else if (menager_izbor == 5)
+                        {
                             cout << "Povratak!" << endl;
                             pause();
                             break;
-                        default:
+                        }
+                        else
+                        {
                             cout << "Krivi unos!" << endl;
                             pause();
                         }
@@ -231,20 +278,87 @@ int main()
                 }
                 else if (menager_izbor == 2)
                     new_user(a, br_user);
-                /*else if (menager_izbor == 3)
+                else if (menager_izbor == 3)
                 {
                     char brisati[51];
-                    cout << "Koga zelite izbrisati:" << endl;
+                    cout << endl << "Koga zelite izbrisati:" << endl;
                     cin.ignore();
                     cin.getline(brisati, 51);
-                    remove_if(a, a + br_user, a->ime==brisati);
-                }*/
+                    remove_user(a, br_user, brisati);
+                    br_user--;
+                }
+                else if (menager_izbor == 4)
+                {
+                    char promijeniti[51];
+                    int i_promjena;
+                    bool ponoviti;
+
+                    do
+                    {
+                        ponoviti = true;
+                        cout << endl << "Koje ime zelite promijeniti:" << endl;
+                        cin.ignore();
+                        cin.getline(promijeniti, 51);
+                        for (int i = 0; i < br_user; i++)
+                        {
+                            if (strcmp(a[i].ime, promijeniti) == 0)
+                            {
+                                i_promjena = i;
+                                ponoviti = false;
+                                break;
+                            }
+                        }
+                    } while (ponoviti);
+
+                    do
+                    {
+                        system("cls");
+                        cout << a[i_promjena].ime << "\t" << a[i_promjena].win << "\t" << a[i_promjena].los << "\t" << a[i_promjena].drw << endl;
+                        ponoviti = false;
+                        cout << endl << "Upisite novo ime:" << endl;
+                        cin.getline(a[i_promjena].ime, 51);
+                        for (int i = 0; i < br_user; i++)
+                        {
+                            if (strcmp(a[br_user].ime, a[i].ime) == 0 && i != i_promjena)
+                            {
+                                cout << "Ime vec postoji, izaberite drugo!" << endl;
+                                ponoviti = true;
+                                pause();
+                            }
+                        }
+                    } while (ponoviti);
+
+                    cout << endl << "Novi broj pobjeda:" << endl;
+                    cin >> a[i_promjena].win;
+                    cout << endl << "Novi broj poraza:" << endl;
+                    cin >> a[i_promjena].los;
+                    cout << endl << "Novi broj izjednacenja:" << endl;
+                    cin >> a[i_promjena].drw;
+                }
+                else if (menager_izbor == 5)
+                {
+                    cout << "Povratak!" << endl;
+                    pause();
+                    break;
+                }
+                else
+                {
+                    cout << "Krivi unos!" << endl;
+                    pause();
+                }
             }
         }
 
         else if (izbor == 4)
         {
-
+            system("cls");
+            sort(a, a + br_user, poWin);
+            cout << "Ime\t|   pobjede\t|     porazi\t|   izjednacenja" << endl;
+            cout << "=============================================================" << endl;
+            for (int i = 0; i < br_user; i++)
+                cout << a[i].ime << "\t|\t" << a[i].win << "\t|\t" << a[i].los << "\t|\t" << a[i].drw << endl;
+            cout << endl;
+            pause();
         }
 
         else if (izbor == 5)
@@ -263,6 +377,11 @@ int main()
 
         }
     }
+
+    users.open("users.bin", ios::out | ios::binary);
+    users.write((char*)&br_user, sizeof(br_user));
+    users.write((char*)&a, sizeof(user)* br_user);
+    users.close();
 
     return 0;
 }
